112f42f1bb008568bf282836b51d07a6f8854225
Xendrra <47497023+Xendrra@users.noreply.github.com>
2019-02-10 16:09:56 +0100

    Update eras.R

--- eras.R
+++ eras.R
@@ -1,14 +1,18 @@
 eras=function(data){
   prvocisla=0
+  k=1
   for (i in 1:length(data)){
-    for (j in 0:length(data)){
-      if (data[i]!=0){
-      prvocisla[i]=data[i]
+    if (data[i]!=0){
+      prvocisla[k]=data[i]
+      k=k+1
+      for (j in 0:length(data)){
+        if (i+j*data[i]<length(data)) {
           data[i+j*data[i]]=0
+        }
       }
     }
   }
-  prvocisla
+  data
 }
 # mam vektor od 2 do 20
 # chci vypsat jenom prvocisla
