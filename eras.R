bba0aab2c720bac39355b26158460ee3b5714880
Xendrra <47497023+Xendrra@users.noreply.github.com>
2019-02-10 22:34:23 +0100

    Update eras.R

--- eras.R
+++ eras.R
@@ -2,21 +2,15 @@ eras=function(data){
   prvocisla=0
   k=1
   for (i in 1:length(data)){
-    if (data[i]!=0){
+    if (data[i]>1){
       prvocisla[k]=data[i]
       k=k+1
       for (j in 0:length(data)){
-        if (i+j*data[i]<length(data)) {
-          data[i+j*data[i]]=0
+        if (i+j*prvocisla[k-1]<=length(data)) {
+          data[i+j*prvocisla[k-1]]=0
         }
       }
     }
   }
-  data
+  prvocisla
 }
-# mam vektor od 2 do 20
-# chci vypsat jenom prvocisla
-# radek 5 by mel ulozit prvni cislo z meho vektoru
-# Radek 6 smaze vsechny celociselne nasobky prvniho cisla 
-# a takhle by to melo jet porad dokola, jenze to nemaze a ja nevim co je spatne
-# (v Rku se indexuje hranatyma zavorkama)
